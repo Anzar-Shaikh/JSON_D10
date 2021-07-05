86644d58857c4ce76181ce9f05009700e5f32a31
anzarshaikh <anzarshaikh373@gmail.com>
2021-07-05 12:45:34 +0530

    Stock Exchange done.

---
+++ StockAccount/src/com/bl/progeammes/date.java
@@ -0,0 +1,16 @@
+package com.bl.progeammes;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class date {
+    public static void main(String[] args) {
+        Date date = new Date();
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println(dateTime);
+
+
+
+    }
+}
